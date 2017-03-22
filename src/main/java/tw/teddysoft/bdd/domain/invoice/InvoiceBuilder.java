f50831c2eabd98a617929140fe83ccf64fb853b1
jay <disc9562@gmail.com>
2017-03-22 21:49:13 +0800

    init

---
+++ src/main/java/tw/teddysoft/bdd/domain/invoice/InvoiceBuilder.java
@@ -0,0 +1,44 @@
+package tw.teddysoft.bdd.domain.invoice;
+
+/**
+ * Created by teddy on 2017/3/9.
+ */
+public class InvoiceBuilder {
+
+    private double vatRate = 0.0;
+    private int taxIncludedPrice = 0;
+   private  int taxExcludedPrice=0;
+    private InvoiceBuilder(){}
+
+    public static InvoiceBuilder newInstance(){
+        return new InvoiceBuilder();
+    }
+
+    public InvoiceBuilder withVatRate(double vatRate) {
+        this.vatRate = vatRate;
+        return this;
+    }
+
+    public InvoiceBuilder withTaxIncludedPrice(int taxIncludedPrice) {
+//        if(this.taxExcludedPrice > 0)
+//            this.taxExcludedPrice = 0;
+        this.taxIncludedPrice = taxIncludedPrice;
+        return this;
+    }
+
+    public Invoice issue() {
+        if(taxIncludedPrice==0 && taxExcludedPrice!=0) {
+            taxIncludedPrice =(int) Math.round(taxExcludedPrice * (1 + vatRate));
+        }
+        return new Invoice(taxIncludedPrice, vatRate,
+                InvoiceCalculator.getTaxExcludedPrice(taxIncludedPrice, vatRate)
+                , InvoiceCalculator.getVAT(taxIncludedPrice, vatRate));
+    }
+
+    public InvoiceBuilder withTaxExcludedPrice(Integer arg1) {
+        if(this.taxIncludedPrice > 0)
+            this.taxIncludedPrice = 0;
+        this.taxExcludedPrice=arg1;
+        return this;
+    }
+}
